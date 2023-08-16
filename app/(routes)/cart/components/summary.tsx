fef677561e3522b818b37f0df3068c1815d5150a
Heri Roque Tello <48292798+HeriRoqueTello@users.noreply.github.com>
2023-08-16 13:57:05 -0500

    Fix labels esp

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -16,7 +16,7 @@ const Summary = () => {
 
   useEffect(() => {
     if (searchParams.get('success')) {
-      toast.success('Payment completed.');
+      toast.success('Pago completado.');
       removeAll();
     }
 
