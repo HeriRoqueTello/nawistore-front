6526be9778b6637c1e32c336159b80ff532eecd0
Heri Roque Tello <48292798+HeriRoqueTello@users.noreply.github.com>
2023-08-17 20:03:53 -0500

    Fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
           <div>
             <div className="flex justify-center text-teal-600 sm:justify-start text-4xl">
-              <img className="w-20" src="https://i.imgur.com/7zgB1IW.jpg" alt="logo" />
+              <img className="w-24" src="https://i.imgur.com/7zgB1IW.jpg" alt="logo" />
             </div>
 
             <p
@@ -141,18 +141,10 @@ const Footer = () => {
                   </a>
                 </li>
 
-                <li>
-                  <a
-                    className="text-gray-700 transition hover:text-gray-700/75"
-                    href="/"
-                  >
-                    Otro
-                  </a>
-                </li>
               </ul>
             </div>
 
-            <div className="text-center sm:text-left">
+            {/* <div className="text-center sm:text-left">
               <p className="text-lg font-medium text-gray-900">Nuestros Servicios</p>
 
               <ul className="mt-8 space-y-4 text-sm">
@@ -192,7 +184,7 @@ const Footer = () => {
                   </a>
                 </li>
               </ul>
-            </div>
+            </div> */}
 
             <div className="text-center sm:text-left">
               <p className="text-lg font-medium text-gray-900">Contactanos</p>
